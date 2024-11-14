cd0f47225f40f03c66099d0908668762064daf40
mwiet <maximilian.von-wietersheim-kramsta@durham.ac.uk>
2024-11-14 17:05:36 +0000

    Updated map with links

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,107 +1,92 @@
 var addressPoints = [
   [
-    "[KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany](https://mwiet.github.io/talks/2023-09-26)",
+    "KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany",
     51.4818111,
     7.2196635
   ],
   [
-    "[Euclid Weak Lensing Meeting, University of Innsbruck, Innsbruck, Austria](https://mwiet.github.io/talks/2024-02-21)",
+    "Euclid Weak Lensing Meeting, University of Innsbruck, Innsbruck, Austria",
     47.2654296,
     11.3927685
   ],
   [
-    "[SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom](https://mwiet.github.io/talks/2024-04-10)",
+    "SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom",
     51.4538022,
     -2.5972985
   ],
   [
-    "[Challenging the standard cosmological model, Royal Society, London, United Kingdom](https://mwiet.github.io/talks/2024-04-15)",
+    "Challenging the standard cosmological model, Royal Society, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[Durham University, Durham, United Kingdom](https://mwiet.github.io/talks/2023-11-16)",
+    "Durham University, Durham, United Kingdom",
     54.666667,
     -1.75
   ],
   [
-    "[Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9, Paris, France](https://mwiet.github.io/talks/2024-10-17)",
+    "Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9, Paris, France",
     48.8534951,
     2.3483915
   ],
   [
-    "[CosmoVerse, Jagiellonian University in Krakow, Krakow, Poland](https://mwiet.github.io/talks/2024-07-10)",
+    "CosmoVerse, Jagiellonian University in Krakow, Krakow, Poland",
     50.0469432,
     19.997153435836697
   ],
   [
-    "[COSMO21, Chania, Greece](https://mwiet.github.io/talks/2024-05-21)",
+    "COSMO21, Chania, Greece",
     35.5120831,
     24.0191544
   ],
   [
-    "[Cosmoparticle Initiative, University College London, London, United Kingdom](https://mwiet.github.io/talks/2020-12-04)",
+    "Cosmoparticle Initiative, University College London, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom](https://mwiet.github.io/talks/2020-03-11)",
+    "Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom",
     55.9533456,
     -3.1883749
   ],
   [
-    "[Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom](https://mwiet.github.io/talks/2024-03-27)",
-    55.9533456,
-    -3.1883749
-  ],
-  [
-    "[Euclid Consortium Meeting, Sapienza Universit\u00e0 di Roma, Rome, Italy](https://mwiet.github.io/talks/2024-06-18)",
+    "Euclid Consortium Meeting, Sapienza Universit\u00e0 di Roma, Rome, Italy",
     41.8933203,
     12.4829321
   ],
   [
-    "[Durham University, Durham, United Kingdom](https://mwiet.github.io/talks/2023-03-09)",
-    54.666667,
-    -1.75
-  ],
-  [
-    "[UK Cosmology Meeting/Ruth Fest, King's College London, London, United Kingdom](https://mwiet.github.io/talks/2024-05-13)",
+    "UK Cosmology Meeting/Ruth Fest, King's College London, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[\u00c9cole Normale Sup\u00e9rieure, Paris, France](https://mwiet.github.io/talks/2022-04-22)",
+    "\u00c9cole Normale Sup\u00e9rieure, Paris, France",
     48.8534951,
     2.3483915
   ],
   [
-    "[KiDS Meeting, National Centre for Nuclear Research, Warsaw, Poland](https://mwiet.github.io/talks/2022-05-18)",
+    "KiDS Meeting, National Centre for Nuclear Research, Warsaw, Poland",
     52.2337172,
     21.071432235636493
   ],
   [
-    "[ICIC, Imperial College London, London, United Kingdom](https://mwiet.github.io/talks/2024-03-12)",
+    "ICIC, Imperial College London, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany](https://mwiet.github.io/talks/2020-11-23)",
-    51.4818111,
-    7.2196635
-  ],
-  [
-    "[Euclid UK Meeting, Royal Astronomical Society, London, United Kingdom](https://mwiet.github.io/talks/2023-12-14)",
+    "Euclid UK Meeting, Royal Astronomical Society, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[Cosmology and Relativity Group, Queen Mary University of London, London, United Kingdom](https://mwiet.github.io/talks/2024-06-27)",
+    "Cosmology and Relativity Group, Queen Mary University of London, London, United Kingdom",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "[KiDS Meeting, E.A. Milne Centre, University of Hull, Hull, United Kingdom](https://mwiet.github.io/talks/2022-05-22)",
-    53.76238625,
-    -0.3301213755327681
+    "KiDS Meeting, E.A. Milne Centre, University of Hull, Hull, United Kingdom",
+    53.7435722,
+    -0.3394758
   ]
 ];
