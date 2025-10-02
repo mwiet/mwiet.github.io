85ac66c65af018d5fcd8887d37c66a4f27782c13
mwiet <maximilian.von-wietersheim-kramsta@durham.ac.uk>
2025-10-02 12:48:09 +0100

    Added anchors to talk map

--- talkmap.py
+++ talkmap.py
@@ -1,47 +1,75 @@
-
-
-# # Leaflet cluster map of talk locations
-#
-# (c) 2016-2017 R. Stuart Geiger, released under the MIT license
-#
-# Run this from the _talks/ directory, which contains .md files of all your talks. 
-# This scrapes the location YAML field from each .md file, geolocates it with
-# geopy/Nominatim, and uses the getorg library to output data, HTML,
-# and Javascript for a standalone cluster map.
-#
-# Requires: glob, getorg, geopy
-
 import glob
 import getorg
 from geopy import Nominatim
+import os
+import traceback
+import time
 
-g = glob.glob("*.md")
+# Use absolute path for the talks directory
+g = glob.glob("/Users/ctsl28/PD/mwiet.github.io/_talks/*.md")
 
-
-geocoder = Nominatim()
-location_dict = {}
-location = ""
-permalink = ""
-title = ""
-
+geocoder = Nominatim(user_agent="talk-map-generator", timeout=20)
+output_dict = {}
 
 for file in g:
     with open(file, 'r') as f:
         lines = f.read()
+        
+        # Extract location
         if lines.find('location: "') > 1:
             loc_start = lines.find('location: "') + 11
             lines_trim = lines[loc_start:]
             loc_end = lines_trim.find('"')
             location = lines_trim[:loc_end]
-                            
-           
-        location_dict[location] = geocoder.geocode(location)
-        print(location, "\n", location_dict[location])
+        else:
+            continue
+
+        # Extract venue
+        if lines.find('venue: "') > 1:
+            venue_start = lines.find('venue: "') + 8
+            lines_trim = lines[venue_start:]
+            venue_end = lines_trim.find('"')
+            venue = lines_trim[:venue_end]
+        else:
+            venue = "Unknown Venue"
+
+        # Extract title
+        if lines.find('title: "') > 1:
+            title_start = lines.find('title: "') + 8
+            lines_trim = lines[title_start:]
+            title_end = lines_trim.find('"')
+            title = lines_trim[:title_end]
+        else:
+            title = "Untitled"
+
+        # Get the filename without extension for the ID
+        talk_id = os.path.splitext(os.path.basename(file))[0]
+
+        # Geocode the location
+        try:
+            geo_location = geocoder.geocode(location)
+            if geo_location:
+                # Create a link with the venue and title
+                link_text = f"{venue}"
+                popup_html = f'<a href="/talks/#{talk_id}" target="_top">{link_text}</a>'
+                
+                # Create a unique key for each talk
+                new_key = f"{popup_html}<br>{venue}, {location}"
+                
+                output_dict[new_key] = geo_location
+            else:
+                print(f"Could not geocode {location}")
+                continue
+        except Exception as e:
+            print(f"Could not geocode {location}: {e}")
+            print(f"Exception type: {type(e)}")
+            traceback.print_exc()
+            continue
+        
+        time.sleep(1)
 
 
+# Generate the map
 m = getorg.orgmap.create_map_obj()
-getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
-
-
-
-
+# Use absolute path for the output folder
+getorg.orgmap.output_html_cluster_map(output_dict, folder_name="/Users/ctsl28/PD/mwiet.github.io/talkmap", hashed_usernames=False)
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,136 +1,176 @@
 var addressPoints = [
   [
-    "Valencia Workshop on the S.S.S. & SIDM, IFIC - CSIC & Universitat de Val\u00e8ncia, Valencia, Spain",
+    "<a href=\"/talks/#2025-06-10\" target=\"_top\">Valencia Workshop on the S.S.S. & SIDM, IFIC - CSIC & Universitat de Val\u00e8ncia</a><br>Valencia Workshop on the S.S.S. & SIDM, IFIC - CSIC & Universitat de Val\u00e8ncia, Valencia, Spain",
     39.4697065,
     -0.3763353
   ],
   [
-    "KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany",
+    "<a href=\"/talks/#2023-09-26\" target=\"_top\">KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum</a><br>KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany",
     51.4818111,
     7.2196635
   ],
   [
-    "Euclid Weak Lensing Meeting, University of Innsbruck, Innsbruck, Austria",
+    "<a href=\"/talks/#2024-02-21\" target=\"_top\">Euclid Weak Lensing Meeting, University of Innsbruck</a><br>Euclid Weak Lensing Meeting, University of Innsbruck, Innsbruck, Austria",
     47.2654296,
     11.3927685
   ],
   [
-    "SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom",
+    "<a href=\"/talks/#2024-04-10\" target=\"_top\">SBI for Galaxy Evolution, University of Bristol</a><br>SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom",
     51.4538022,
     -2.5972985
   ],
   [
-    "Challenging the standard cosmological model, Royal Society, London, United Kingdom",
+    "<a href=\"/talks/#2024-04-15\" target=\"_top\">Challenging the standard cosmological model, Royal Society</a><br>Challenging the standard cosmological model, Royal Society, London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "Beyond 2pt Statistics Meet Survey Systematics, Kavli IPMU, Tokyo, Japan",
+    "<a href=\"/talks/#2025-09-17\" target=\"_top\">Beyond 2pt Statistics Meet Survey Systematics, Kavli IPMU</a><br>Beyond 2pt Statistics Meet Survey Systematics, Kavli IPMU, Tokyo, Japan",
     35.6768601,
     139.7638947
   ],
   [
-    "Durham University, Durham, United Kingdom",
+    "<a href=\"/talks/#2023-11-16\" target=\"_top\">Durham University</a><br>Durham University, Durham, United Kingdom",
     54.666667,
     -1.75
   ],
   [
-    "Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9, Paris, France",
+    "<a href=\"/talks/#2024-10-17\" target=\"_top\">Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9</a><br>Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9, Paris, France",
     48.8534951,
     2.3483915
   ],
   [
-    "CosmoVerse, Jagiellonian University in Krakow, Krakow, Poland",
-    50.0619474,
-    19.9368564
+    "<a href=\"/talks/#2024-07-10\" target=\"_top\">CosmoVerse, Jagiellonian University in Krakow</a><br>CosmoVerse, Jagiellonian University in Krakow, Krakow, Poland",
+    50.0469432,
+    19.9971534
   ],
   [
-    "Open University, Milton Keynes, United Kingdom",
+    "<a href=\"/talks/#2025-05-14\" target=\"_top\">Open University</a><br>Open University, Milton Keynes, United Kingdom",
     52.0406502,
     -0.7594092
   ],
   [
-    "Euclid Consortium Meeting, Leiden, Netherlands",
+    "<a href=\"/talks/#2025-03-25\" target=\"_top\">Euclid Consortium Meeting</a><br>Euclid Consortium Meeting, Leiden, Netherlands",
     52.1518157,
     4.4811089
   ],
   [
-    "COSMO21, Chania, Greece",
+    "<a href=\"/talks/#2024-05-21\" target=\"_top\">COSMO21</a><br>COSMO21, Chania, Greece",
     35.5120831,
     24.0191544
   ],
   [
-    "Cosmoparticle Initiative, University College London, London, United Kingdom",
+    "<a href=\"/talks/#2020-12-04\" target=\"_top\">Cosmoparticle Initiative, University College London</a><br>Cosmoparticle Initiative, University College London, London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "ASTRODAT, Durham University, Durham, United Kingdom",
+    "<a href=\"/talks/#2024-11-08\" target=\"_top\">Durham University</a><br>Durham University, Durham, United Kingdom",
     54.666667,
     -1.75
   ],
   [
-    "Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom",
-    55.9533456,
-    -3.1883749
+    "<a href=\"/talks/#2025-05-25\" target=\"_top\">SBI for Galaxy Evolution, University of Bristol</a><br>SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom",
+    51.4538022,
+    -2.5972985
   ],
   [
-    "Euclid Consortium Meeting, Sapienza Universit\u00e0 di Roma, Rome, Italy",
-    41.8933203,
-    12.4829321
-  ],
-  [
-    "National Astronomy Meeting (NAM), Durham University, Durham, United Kingdom",
-    54.666667,
-    -1.75
-  ],
-  [
-    "UK Cosmology Meeting/Ruth Fest, King's College London, London, United Kingdom",
-    51.5074456,
-    -0.1277653
-  ],
-  [
-    "Max Planck Institute for Astrophysics, Garching, Germany",
-    48.2513878,
-    11.6509662
-  ],
-  [
-    "LFI in Paris, \u00c9cole Normale Sup\u00e9rieure, Paris, France",
+    "<a href=\"/talks/#2024-10-16\" target=\"_top\">Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9</a><br>Institut d'Astrophysique de Paris, Sorbonne Universit\u00e9, Paris, France",
     48.8534951,
     2.3483915
   ],
   [
-    "KiDS Meeting, National Centre for Nuclear Research, Warsaw, Poland",
+    "<a href=\"/talks/#2025-09-11\" target=\"_top\">ASTRODAT, Durham University</a><br>ASTRODAT, Durham University, Durham, United Kingdom",
+    54.666667,
+    -1.75
+  ],
+  [
+    "<a href=\"/talks/#2020-03-11\" target=\"_top\">Institute for Astronomy, University of Edinburgh</a><br>Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom",
+    55.9533456,
+    -3.1883749
+  ],
+  [
+    "<a href=\"/talks/#2024-03-27\" target=\"_top\">Institute for Astronomy, University of Edinburgh</a><br>Institute for Astronomy, University of Edinburgh, Edinburgh, United Kingdom",
+    55.9533456,
+    -3.1883749
+  ],
+  [
+    "<a href=\"/talks/#2024-06-18\" target=\"_top\">Euclid Consortium Meeting, Sapienza Universit\u00e0 di Roma</a><br>Euclid Consortium Meeting, Sapienza Universit\u00e0 di Roma, Rome, Italy",
+    41.8933203,
+    12.4829321
+  ],
+  [
+    "<a href=\"/talks/#2025-05-26\" target=\"_top\">SBI for Galaxy Evolution, University of Bristol</a><br>SBI for Galaxy Evolution, University of Bristol, Bristol, United Kingdom",
+    51.4538022,
+    -2.5972985
+  ],
+  [
+    "<a href=\"/talks/#2023-03-09\" target=\"_top\">Durham University</a><br>Durham University, Durham, United Kingdom",
+    54.666667,
+    -1.75
+  ],
+  [
+    "<a href=\"/talks/#2025-07-07\" target=\"_top\">National Astronomy Meeting (NAM), Durham University</a><br>National Astronomy Meeting (NAM), Durham University, Durham, United Kingdom",
+    54.666667,
+    -1.75
+  ],
+  [
+    "<a href=\"/talks/#2024-05-13\" target=\"_top\">UK Cosmology Meeting/Ruth Fest, King's College London</a><br>UK Cosmology Meeting/Ruth Fest, King's College London, London, United Kingdom",
+    51.5074456,
+    -0.1277653
+  ],
+  [
+    "<a href=\"/talks/#2025-05-23\" target=\"_top\">Max Planck Institute for Astrophysics</a><br>Max Planck Institute for Astrophysics, Garching, Germany",
+    48.2513878,
+    11.6509662
+  ],
+  [
+    "<a href=\"/talks/#2022-04-22\" target=\"_top\">LFI in Paris, \u00c9cole Normale Sup\u00e9rieure</a><br>LFI in Paris, \u00c9cole Normale Sup\u00e9rieure, Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "<a href=\"/talks/#2025-03-26\" target=\"_top\">Euclid Consortium Meeting</a><br>Euclid Consortium Meeting, Leiden, Netherlands",
+    52.1518157,
+    4.4811089
+  ],
+  [
+    "<a href=\"/talks/#2022-05-18\" target=\"_top\">KiDS Meeting, National Centre for Nuclear Research</a><br>KiDS Meeting, National Centre for Nuclear Research, Warsaw, Poland",
     52.2333742,
     21.0711489
   ],
   [
-    "ICIC, Imperial College London, London, United Kingdom",
+    "<a href=\"/talks/#2024-03-12\" target=\"_top\">ICIC, Imperial College London</a><br>ICIC, Imperial College London, London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "Euclid UK Meeting, Royal Astronomical Society, London, United Kingdom",
+    "<a href=\"/talks/#2020-11-23\" target=\"_top\">KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum</a><br>KiDS Meeting, German Centre for Cosmological Lensing, Ruhr-Universit\u00e4t Bochum, Bochum, Germany",
+    51.4818111,
+    7.2196635
+  ],
+  [
+    "<a href=\"/talks/#2023-12-14\" target=\"_top\">Euclid UK Meeting, Royal Astronomical Society</a><br>Euclid UK Meeting, Royal Astronomical Society, London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "Newcastle University, Newcastle, United Kingdom",
+    "<a href=\"/talks/#2025-01-09\" target=\"_top\">Newcastle University</a><br>Newcastle University, Newcastle, United Kingdom",
     54.9738474,
     -1.6131572
   ],
   [
-    "Cosmology and Relativity Group, Queen Mary University of London, London, United Kingdom",
+    "<a href=\"/talks/#2024-06-27\" target=\"_top\">Cosmology and Relativity Group, Queen Mary University of London</a><br>Cosmology and Relativity Group, Queen Mary University of London, London, United Kingdom",
     51.5074456,
     -0.1277653
   ],
   [
-    "University of Hong Kong, Hong Kong, China",
+    "<a href=\"/talks/#2025-04-07\" target=\"_top\">University of Hong Kong</a><br>University of Hong Kong, Hong Kong, China",
     22.350627,
     114.1849161
   ],
   [
-    "KiDS Meeting, E.A. Milne Centre, University of Hull, Hull, United Kingdom",
+    "<a href=\"/talks/#2022-05-22\" target=\"_top\">KiDS Meeting, E.A. Milne Centre, University of Hull</a><br>KiDS Meeting, E.A. Milne Centre, University of Hull, Hull, United Kingdom",
     53.7623862,
     -0.3301214
   ]
